e894d7a027b09a9bb4dc6bb85536fea8df35acb4
YunkaiLiu <lyk7458@outlook.com>
2018-12-09 17:23:37 +0000

    生成user模型

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  #属性有效性验证
+  validates :name, presence: true, length: {maximum: 50}
+end
