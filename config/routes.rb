f29aae4f64f54525023fd098c081a65bf6b7b391
YunkaiLiu <lyk7458@outlook.com>
2018-12-10 03:58:21 +0000

    登录\注册\静态页面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
+  
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
