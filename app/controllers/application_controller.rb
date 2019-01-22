b1a386efda395cb0a6c7e30ce82d7366bdb67d7d
YunkaiLiu <lyk7458@outlook.com>
2019-01-22 12:23:58 +0000

    1-22-20:23

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :wechatid, :phone, :studentid, :academy, :sex])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :wechatid, :phone, :studentid, :academy, :sex, :password, :password_confirmation, :current_password])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
-  devise_for :users,
+  devise_for :users, 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   path: '',
   path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'}, 
